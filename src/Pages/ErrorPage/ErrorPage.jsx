18568966562c1b3d972bb7856ea89de533cbebee
unknown <emonhossen1000@gmail.com>
2023-10-21 17:22:14 +0600

    errorpage added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 const ErrorPage = () => {
     return (
         <div>
-            <h2 className="text-3xl">ERROR PAGE</h2>
+            <Navbar></Navbar>
+            <div className="flex flex-col md:flex-col lg:flex-row gap-5  items-center">
+            <img className="mt-5 " src="https://i.ibb.co/3srzkM7/b8fb705699100d965d1ede440f63bd35.jpg" alt="" />
+            <Link to="/" ><button className="btn btn-outline btn-info">Go Home</button></Link>
+            </div>
         </div>
     );
 };
